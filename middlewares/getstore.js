b0ecf52ef683f43908663dde157373fd0dffd48c
dillionmegida <dillionmegida@gmail.com>
2020-10-11 09:42:22 +0100

    fix: accept requests from main app correctly

--- middlewares/getstore.js
+++ middlewares/getstore.js
@@ -3,7 +3,11 @@ const Store = require("../models/Store");
 const path = require("path");
 
 module.exports = async function getStore(req, res, next) {
-  const { store_name } = req.headers;
+  const { store_name, main = null } = req.headers;
+
+  if (main !== null)
+    // then API requests are coming from the main app
+    return next();
 
   // if (subdomain === "admin") {
   //   req.admin = true;
@@ -12,9 +16,8 @@ module.exports = async function getStore(req, res, next) {
 
   const store = await Store.findOne({ shortname: store_name });
 
-  // console.log({store_name, store})
-
   // then store does not exist
+  // TODO redirect instead of sending file
   if (!store) return res.sendFile(path.join(__dirname, "../store404.html"));
 
   req.store_id = store._id;
