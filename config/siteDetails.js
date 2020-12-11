1ac21e022a0c60e71d50a39121e8e24284cdf1a4
dillionmegida <dillionmegida@gmail.com>
2020-12-11 14:11:10 +0100

    fix: redirect to no store page if store not found

--- config/siteDetails.js
+++ config/siteDetails.js
@@ -3,4 +3,5 @@ module.exports = {
   motto:
     "SkulMart is an online market for connecting buyers and sellers in school settings",
   email: "support@skulmart.com",
+  domain: 'https://skulmart.com'
 };
--- middlewares/getstore.js
+++ middlewares/getstore.js
@@ -1,5 +1,5 @@
 const Store = require("../models/Store");
-const path = require("path");
+const siteDetails = require("../config/siteDetails");
 
 module.exports = async function getStore(req, res, next) {
   const { store_name, main = null } = req.headers;
@@ -16,8 +16,8 @@ module.exports = async function getStore(req, res, next) {
   const store = await Store.findOne({ shortname: store_name });
 
   // then store does not exist
-  // TODO redirect instead of sending file
-  // if (!store) return res.sendFile(path.join(__dirname, "../store404.html"));
+  if (!store)
+    res.redirect(siteDetails.domain + "/no-store?store=" + store_name);
 
   req.store_id = store._id;
 
