685fdf9d9fa8306f35f0aa1d8e4149849495ba5b
dillionmegida <dillionmegida@gmail.com>
2021-01-05 10:29:14 +0100

    feat: add update cart api

--- api/controllers/buyers/index.ts
+++ api/controllers/buyers/index.ts
@@ -1,4 +1,5 @@
 export { default as addToCart } from "./addToCart";
+export { default as updateItemInCart } from "./updateItemInCart";
 export { default as removeFromCart } from "./removeFromCart";
 export { default as getCart } from "./getCart";
 export { default as initializeBuyProduct } from "./initializeBuyProduct";
--- models/Buyer.ts
+++ models/Buyer.ts
@@ -26,6 +26,7 @@ const BuyerSchema: Schema = new Schema(
     cart: [
       {
         type: Schema.Types.ObjectId,
+        required: true,
         ref: "Cart",
       },
     ],
