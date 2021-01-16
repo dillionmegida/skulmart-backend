1d64fe6eeec87814f15d5b9ac2acbc36d2b3a608
dillionmegida <dillionmegida@gmail.com>
2021-01-16 05:36:43 +0100

    populate review in getOrder

--- api/controllers/orders/getOrder.ts
+++ api/controllers/orders/getOrder.ts
@@ -4,7 +4,9 @@ export default async function getOrder(req: any, res: any) {
   const { id } = req.params;
 
   try {
-    const order = await Order.findById(id).populate("product");
+    const order = await Order.findById(id)
+      .populate("product")
+      .populate("review");
     res.json({
       order,
     });
